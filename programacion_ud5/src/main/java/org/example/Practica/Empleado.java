506ff4f05e3629f5d07dd19481f938dff5c993f0
Adrian Geada <adrigeada@gmail.com>
2026-02-05 16:11:44 +0100

    p

--- programacion_ud5/src/main/java/org/example/Practica/Empleado.java
+++ programacion_ud5/src/main/java/org/example/Practica/Empleado.java
@@ -56,14 +56,6 @@ public class Empleado {
 
     public String setCargo(String cargo) {
 
-<<<<<<< HEAD
-
-
-=======
-      
->>>>>>> parent of 68ace0d (.)
-        if (cargo.equals("director") || cargo.equals("tecnico") || cargo.equals("presentador") || cargo.equals("colaborador")){
-
         if (Arrays.asList(POSIBLES_CARGOS).contains(cargo)){
             return cargo;
         }else {
